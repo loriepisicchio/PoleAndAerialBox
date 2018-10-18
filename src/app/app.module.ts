66968773fcd3f1ca080c25457140a84b1e078a09
Lorie Pisicchio <lorie.pisicchio@kelkoogroup.com>
2018-10-18 09:11:09 +0200

    WIP header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentPanelComponent } from './content-panel/content-panel.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContentPanelComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
